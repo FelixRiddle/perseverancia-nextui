ce96e097dbd55793edb68c51fe2dbcff35e298ea
Felix <fedecraft3312@gmail.com>
2024-08-15 09:46:24 -0300

    Release ready

--- src/lib/config/apiUrl.ts
+++ src/lib/config/apiUrl.ts
@@ -1,8 +1,15 @@
-export const API_PORT = process.env.API_PORT || 3010;
-
 /**
  * Api url
  */
 export default function apiUrl() {
-	return `http://localhost:${API_PORT}`;
+	const API_PORT = process.env.API_PORT || 3010;
+	const API_HOST = process.env.API_HOST || 'localhost';
+	const API_DOMAIN = process.env.API_DOMAIN;
+	const API_PATH = process.env.API_PATH || "";
+	
+	if(API_DOMAIN) {
+		return `https://${API_DOMAIN}${API_PATH}`;
+	}
+	
+	return `http://${API_HOST}:${API_PORT}${API_PATH}`;
 }
